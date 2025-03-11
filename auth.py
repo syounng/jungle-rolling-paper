484571307ca713e8a8f9cf8644891f3d2949a1e4
jkh1447 <jkh1447@gmail.com>
2025-03-12 01:47:35 +0900

    feat:로그인 메인화면 연결

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 # jwt 토큰 생성, 토큰 검증, 로그인/회원가입을 처리하는 파일
 from database import db
-from flask import Blueprint, jsonify, request
-from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token
+from flask import Blueprint, jsonify, request, make_response, redirect, url_for
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies
 
 bp = Blueprint('bp_auth', __name__)
 
@@ -16,10 +16,14 @@ def login():
     user = db.users.find_one({'id': id, 'pwd': pwd}) #일치하는 유저 찾기
     if user:
         #맞았을때, 토큰발행
-
         access_token = create_access_token(identity=id)
         refresh_token = create_refresh_token(identity=id)
-        return jsonify(result = 'success', access_token=access_token, refresh_token=refresh_token)
+        resp_obj = make_response(redirect(url_for('main')))
+        set_access_cookies(resp_obj, access_token)
+        set_refresh_cookies(resp_obj, refresh_token)
+
+        
+        return resp_obj
     else:
         #틀렸을때
         return jsonify(result = 'failure')
