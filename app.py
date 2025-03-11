12dc1121d442ce62429692d30293fdd8d6c10b31
soyoung <soyoung@soyoungui-MacBookAir.local>
2025-03-12 00:22:30 +0900

    feat: 회원가입 화면 개발 - 이름, 아이디, 비밀번호, 비밀번호 확인 Input을 받고 - 완료 버튼을 누르면 각각의 값이 유효한지 검증합니다. - 정책에 맞지 않는 값이 입력되었으면 alert창을 띄워 다시 입력하게 합니다.

--- app.py
+++ app.py
@@ -27,6 +27,9 @@ def home():
 def login():
    return render_template('login.html')
 
+@app.route('/join')
+def join():
+   return render_template('join.html')
 
 if __name__ == '__main__':  
    app.run('0.0.0.0', port=5001, debug=True)
