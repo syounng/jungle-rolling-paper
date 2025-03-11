27ea9e1732ecff6218fd052b8f7341c1c62f08e6
jkh1447 <jkh1447@gmail.com>
2025-03-11 19:25:02 +0900

    feat:메인페이지, 마이페이지 개발

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-# 애플리케이션 실행 파일
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.jungle9
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   users = db.users.find()
+   return render_template('main.html', users = users)
+
+
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0', port=5000, debug=True)
+
 
