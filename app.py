da44b0541495ea3dcd94d6eeed0bab603a7f6fe0
jkh1447 <jkh1447@gmail.com>
2025-03-12 01:35:48 +0900

    feat:로그인 메인화면 연결

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
-from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 from auth import bp
 from database import db
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 #secretkey 설정
 app.config['JWT_SECRET_KEY'] = 'secret_key'
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']  # 쿠키에서 토큰을 추출
 jwt = JWTManager(app)
 
 #블루프린트 등록
@@ -16,12 +17,19 @@ app.register_blueprint(bp, url_prefix='/auth')
 
 @app.route('/')
 def home():
-   users = db.users.find()
-   return render_template('login.html', users = users)
-
-@app.route('/login')
-def login():
    return render_template('login.html')
+
+
+@app.route('/main', methods=['GET'])
+@jwt_required()
+def main():
+#    token_receive = request.cookies.get('access_token')
+
+    current_user = get_jwt_identity()
+    users = db.users.find()
+    current_user = db.users.find_one({'id': current_user})['name']
+    print(current_user)
+    return render_template('main.html', users = users, current_user = current_user)
 
 
 if __name__ == '__main__':  
