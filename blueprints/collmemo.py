66796af0d6b243b8892b6003ad9cbda1611368da
jkh1447 <jkh1447@gmail.com>
2025-03-12 17:27:46 +0900

    feat:내 롤링페이퍼페이지 인가 인증

--- blueprints/collmemo.py
+++ blueprints/collmemo.py
@@ -1,45 +1,74 @@
-from flask import Blueprint, jsonify, request, render_template
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from database import db  # database.py에서 MongoDB 객체 가져오기
+from flask import Flask, jsonify, request, render_template, Blueprint, redirect, url_for
+from pymongo import MongoClient
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity, decode_token
 
-# Blueprint 객체 생성
-collmemo_bp = Blueprint('collmemo', __name__, url_prefix='/collmemo')
+# Flask 앱 설정
+bp_callmemo = Blueprint('bp_callmemo', __name__)
 
-# 특정 동료의 메모 가져오기 (JWT 필요)
-@collmemo_bp.route('/memos/<to_id>', methods=['GET'])
-@jwt_required()  #JWT 보호 추가
+# MongoDB 연결 (로컬 DB 사용)
+client = MongoClient("mongodb://localhost:27017/")
+db = client.jungle9
+# ✅ 1. 특정 동료의 메모 가져오기
+@bp_callmemo.route('/memos/<to_id>', methods=['GET'])
 def get_memos(to_id):
+    print(to_id)
     """ 특정 동료(to_id)에게 남긴 메모 조회 """
-    memos = list(db.memos.find({"to_id": to_id}, {"_id": 0}))  # _id 제외
+    id = db.users.find_one({'name':to_id})['id']
+    memos = list(db.memos.find({"to_id": id}, {"_id": 0}))  # _id는 제외하고 가져오기
     return jsonify(memos=memos), 200
 
-# ✅ 메모 등록 (JWT 필요)
-@collmemo_bp.route('/regist/memo', methods=['POST'])
-@jwt_required()  #JWT 보호 추가
+
+
+# ✅ 2. 메모 등록 (JWT 없이 누구나 가능)
+@bp_callmemo.route('/regist/memo', methods=['POST'])
 def regist_memo():
-    """ 메모 저장 (로그인한 사용자만 가능) """
-    data = request.get_json()
-    to_id = data.get("to_id")
-    nickname = data.get("nickname", "익명")  # 닉네임이 없으면 "익명" 사용
-    content = data.get("content")
-    from_id = get_jwt_identity()  # ✅ JWT에서 현재 사용자 ID 가져오기
+    cookies = request.cookies  # 요청에서 받은 쿠키 확인
+    access_token = cookies.get("access_token_cookie")  # 특정 쿠키 값 확인
+    
+    if access_token:
+        decoded_token = decode_token(access_token)
+        from_id = decoded_token.get('sub')
+        data = request.get_json()
 
-    # 필수값 체크
-    if not to_id or not content:
-        return jsonify({"msg": "to_id와 content는 필수 입력 항목입니다."}), 400
+        tmp = data.get("to_id")
+        received_to_id = db.users.find_one({'name': tmp})['id']
 
-    # MongoDB에 저장
-    db.memos.insert_one({
-        "from_id": from_id,  # JWT 사용자 ID 저장
-        "to_id": to_id,
-        "nickname": nickname,
-        "content": content
-    })
 
-    return jsonify({"msg": "메모 등록 완료!"}), 201
+        to_id = received_to_id
+        nickname = data.get("nickname", "익명")  # 닉네임이 없으면 "익명" 사용
+        content = data.get("content")
+        from_id = from_id  # from_id는 고정된 값
 
-# 특정 동료의 롤링페이퍼 화면 (JWT 필요)
-@collmemo_bp.route('/rollingpaper/<to_id>')
-@jwt_required()  # JWT 보호 추가
+        # 필수값 체크
+        if not to_id or not content:
+            return jsonify({"msg": "to_id와 content는 필수 입력 항목입니다."}), 400
+
+        # MongoDB에 저장
+        db.memos.insert_one({
+            "from_id": from_id,
+            "to_id": to_id,
+            "nickname": nickname,
+            "content": content,
+            "name": tmp,
+            "quiz": 'default',
+        })
+
+        return jsonify({"msg": "메모 등록 완료!"}), 201
+        
+    else:
+        return jsonify({"msg": "쿠키가 포함되지 않았음"}), 400
+    # current_user = get_jwt_identity()
+    """ 메모 저장 (누구나 가능) """
+
+# ✅ 3. 특정 동료의 롤링페이퍼 화면
+@bp_callmemo.route('/rollingpaper/<to_id>')
+@jwt_required()
 def rolling_paper(to_id):
-    return render_template('colleagueList.html', to_id=to_id)
+    current_user = get_jwt_identity()
+    real_id = db.users.find_one({'name':to_id})['id']
+
+    if current_user == real_id:
+        return redirect(url_for('mymemo.my_rolling_paper'))
+    else:
+        return render_template('colleagueList.html', to_id=to_id)
+
--- blueprints/mymemo.py
+++ blueprints/mymemo.py
@@ -3,15 +3,15 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from database import db  # database.py에서 MongoDB 객체 가져오기
 
 # Blueprint 객체 생성
-mymemo_bp = Blueprint('mymemo', __name__, url_prefix='/mymemo')
+mymemo_bp = Blueprint('mymemo', __name__)
 
 # 내가 받은 메모 가져오기 (JWT 필요)
-@mymemo_bp.route('/memos/me', methods=['GET'])
+@mymemo_bp.route('/memos/me/<to_id>', methods=['GET'])
 @jwt_required()  # JWT 보호 추가
-def get_my_memos():
+def get_my_memos(to_id):
     """ 현재 로그인한 사용자가 받은 메모 조회 """
     current_user = get_jwt_identity()  #현재 로그인한 사용자 ID 가져오기
-
+    
     # MongoDB에서 해당 사용자의 메모만 조회
     memos = list(db.memos.find({"to_id": current_user}, {"_id": 0}))
 
@@ -22,4 +22,5 @@ def get_my_memos():
 @jwt_required()  # JWT 보호 추가
 def my_rolling_paper():
     current_user = get_jwt_identity()  #현재 로그인한 사용자 ID 가져오기
-    return render_template('myList.html', to_id=current_user)
+    user_name = db.users.find_one({'id':current_user})['name']
+    return render_template('myList.html', to_id=user_name)
