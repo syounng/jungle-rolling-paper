c52c48ff574c9419f7fbc6a63a26642b3046bc9f
jkh1447 <jkh1447@gmail.com>
2025-03-12 11:06:49 +0900

    feat:로그인 메인화면 연결

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 # jwt 토큰 생성, 토큰 검증, 로그인/회원가입을 처리하는 파일
 from database import db
-from flask import Blueprint, jsonify, request, make_response, redirect, url_for
-from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies
+from flask import Blueprint, jsonify, request, make_response, redirect, url_for, render_template
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, jwt_required, get_jwt_identity
 
 bp = Blueprint('bp_auth', __name__)
 
@@ -18,15 +18,18 @@ def login():
         #맞았을때, 토큰발행
         access_token = create_access_token(identity=id)
         refresh_token = create_refresh_token(identity=id)
-        resp_obj = make_response(redirect(url_for('main')))
-        set_access_cookies(resp_obj, access_token)
-        set_refresh_cookies(resp_obj, refresh_token)
+        resp_obj = make_response(jsonify(result = 'success'))
 
+        resp_obj.set_cookie('access_token_cookie', access_token, httponly=True, secure=True, samesite="Lax")
+        resp_obj.set_cookie('refresh_token_cookie', refresh_token, httponly=True, secure=True, samesite="Lax")
+        
         
         return resp_obj
     else:
         #틀렸을때
         return jsonify(result = 'failure')
+    
+
 
 @bp.route('/protected', methods=['GET'])
 def protected():
