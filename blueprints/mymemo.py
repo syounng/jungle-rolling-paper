5d935506f42799326f5b74332020099c393d4609
jkh1447 <jkh1447@gmail.com>
2025-03-13 02:41:21 +0900

    all

--- blueprints/mymemo.py
+++ blueprints/mymemo.py
@@ -1,27 +1,34 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, request, render_template
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from database import db  # database.py에서 MongoDB 객체 가져오기
-
-# Blueprint 객체 생성
+from database import db
+from bson import ObjectId
 mymemo_bp = Blueprint('mymemo', __name__)
-
-# 내가 받은 메모 가져오기 (JWT 필요)
 @mymemo_bp.route('/memos/me/<to_id>', methods=['GET'])
 @jwt_required()
 def get_my_memos(to_id):
     """ 현재 로그인한 사용자가 받은 메모 조회 """
     current_user = get_jwt_identity()
     # :흰색_확인_표시: 해당 사용자의 메모 조회
-    memos = list(db.memos.find({"to_id": current_user}, {"_id": 1, "nickname": 1, "name": 1, "content": 1}))
+    memos = list(db.memos.find({"to_id": current_user}, {"_id": 1, "nickname": 1, "name": 1, "content": 1, "quiz": 1}))
     # ObjectId를 문자열로 변환
     for memo in memos:
         memo["_id"] = str(memo["_id"])
     return jsonify(memos=memos), 200
-
-# 내가 받은 롤링페이퍼 페이지 (JWT 필요)
-@mymemo_bp.route('/rollingpaper/me')
-@jwt_required()  # JWT 보호 추가
+@mymemo_bp.route('/rollingpaper/me', methods=['GET'])
+@jwt_required()
 def my_rolling_paper():
-    current_user = get_jwt_identity()  #현재 로그인한 사용자 ID 가져오기
-    user_name = db.users.find_one({'id':current_user})['name']
+    """ 내가 받은 롤링페이퍼 페이지 """
+    current_user = get_jwt_identity()
+    user = db.users.find_one({'id': current_user})
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    user_name = user['name']
     return render_template('myList.html', to_id=user_name)
+
+@mymemo_bp.route('/updateQuizState', methods=["POST"])
+def updateQuizState():
+    current_id = request.form.get('id')
+    quiz_state = request.form.get('quizState')
+
+    db.memos.update_one({'_id': ObjectId(current_id)}, {'$set': {'quiz': quiz_state}})
+    return jsonify({'result': quiz_state})
